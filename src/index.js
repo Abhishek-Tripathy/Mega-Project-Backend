e1aa7d239af41a9f552d92751e752b647ea0795b
Abhishek-Tripathy <134616533+Abhishek-Tripathy@users.noreply.github.com>
2024-08-13 12:20:02 +0530

    Utility parts added

--- src/index.js
+++ src/index.js
@@ -5,6 +5,15 @@ dotenv.config({path: './env'})
 
 
 connectDB()
+.then(() => [
+   app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+      
+   })
+])
+.catch((error) => {
+   console.log("MongoDB connection failed" ,error);
+})
 
 
 
